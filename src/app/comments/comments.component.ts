4b4da0305c1bf6ee06cdcbba8127ab1d5676f31b
NotProfessional <igor.karpyuk00@gmail.com>
2019-07-12 15:08:46 +0300

    commit

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
-
+url = 'C:\\Users\\haker\\OneDrive\\Робочий стіл\\Project\\image\\film1.jpg';
   constructor() { }
 
   ngOnInit() {}
